87c4efdaf06f7c3c0754afd6c09569fc7663f90b
Goviindsain <gsksain267@gmail.com>
2024-07-31 22:16:59 +0530

    Add the header of project

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      homepage
+    <div className='text-red-400'> 
+home
     </div>
   )
 }
