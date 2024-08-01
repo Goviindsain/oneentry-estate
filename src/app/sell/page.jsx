b064a93f5e0bb3a68776d9a49b117c73c49a99e3
Goviindsain <gsksain267@gmail.com>
2024-08-01 10:05:22 +0530

    complete rent, sale, listings and about pages

--- src/app/sell/page.jsx
+++ src/app/sell/page.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import ListingItem from '@/components/ListingItem';
+import { defineOneEntry } from 'oneentry';
 
-export default function page() {
+const { Products } = defineOneEntry('https://govindestate.oneentry.cloud', {
+  token: process.env.NEXT_PUBLIC_ONEENTRY_TOKEN,
+});
+
+export default async function Sell() {
+  const sellListings = await Products.getProductsByPageUrl('sell');
   return (
     <div>
-      sell
+      <h1 className='text-3xl font-semibold text-slate-600 text-center my-10'>
+        Recent places for sell
+      </h1>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
+        {sellListings && sellListings.length > 0 && (
+          <div className='flex flex-wrap gap-4'>
+            {sellListings.map((listing) => (
+              <ListingItem listing={listing} key={listing.id} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
